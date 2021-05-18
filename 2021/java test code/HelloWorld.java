62412fd45fc9f2c2d733137839a4c3a11366d566
TheUnknown1221 <80987328+TheUnknown1221@users.noreply.github.com>
2021-05-18 14:55:33 +0100

    Create HelloWorld.java

---
+++ 2021/java test code/HelloWorld.java
@@ -0,0 +1,24 @@
+package HelloWorld;
+import java.utils:scanner;
+
+
+{
+
+	public class main(String[] args[]) 
+
+{		int new_num = 0;
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		System.out.println("test? ");
+		if (name.Equals("bob")) System.out.print("hello " + name.strip() + " nice to see your back"); else System.out.println("hello " + name.strip());
+		System.out.println("pick a number any number: ");
+		new_num = scanner.nextLine();
+		if new_num<=0; new_num = 4;
+		while(int i, i<=new_num, i++)
+		{
+			System.out.println("this is test number " + i);
+		}
+		System.out.println("done")
+		scanner.close();
+}
+}
