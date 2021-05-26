5ccf2e76fd1e438b9a54593110667cc4f0930b86
TheUnknown1221 <80987328+TheUnknown1221@users.noreply.github.com>
2021-05-26 22:30:34 +0100

    NEW things
    added a temp list when picking a random bird making it easier to deal with values instead if going through an array

--- 2021/collage/main.py
+++ 2021/collage/main.py
@@ -26,10 +26,12 @@ class person:
             self.name = "jackson"
 
         self.money = 0
-        # self.menupicker()
         self.birds()
-        listofNEWbirds = self.birds()
-        print(listofNEWbirds)
+        self.listofNEWbirds = self.birds()
+        self.menupicker()
+
+        # temp varables
+        self.weapondamage = 10 
 
 
 
@@ -44,8 +46,16 @@ class person:
             temp_list = [items, temp_worth]
             listofNEWbirds.append(temp_list)
         return listofNEWbirds
-    
 
+
+    # selects a random bird and makes is it esier to select the values from a temp list 
+    # rather thn gogin through an array
+    def randombird(self):
+        temp = random.choice(self.listofNEWbirds)
+         # random ==== [name, wight, cost]
+        randombird = [temp[0], int(numpy.round(temp[1] / 2.5)), temp[1]]
+        return randombird
+    
 
     """
     This is where the user functions begin
@@ -67,10 +77,20 @@ class person:
 
         while True:
             temp_user = str(input("terminal: "))
+            temp_user = temp_user.strip()
 
             if len(temp_user) <= 2:
                 print("sorry could not find what your looking for")
             
+            elif temp_user == "debug":
+                #this is to test any small code without creating a new function and running it 
+                # print(int(numpy.round(temp_worth / 2.5)))
+                randombird = self.randombird()
+                print(randombird)
+                print(f"name: {randombird[0]},\ncost: ${randombird[1]},\nwight: {randombird[2]}Kg")
+                
+    
+
             else:
                 print(f"sorry {self.name}, I could not find ({temp_user.strip()}) anywhere")
 
@@ -78,4 +98,5 @@ class person:
 if __name__ == "__main__":
     listofbirds = openlist()
     user = person(str(input("user: ")), listofbirds)
+
 
